eea806b081b7a27ea5edb79ada7fc75c2ed2593c
heno7 <105067953+heno7@users.noreply.github.com>
2025-03-27 12:34:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-import { createServer } from 'http';
-import { createReadStream } from 'fs';
-import { WebSocketServer } from 'ws';
+import { createServer } from "http";
+import { createReadStream } from "fs";
+import { WebSocketServer } from "ws";
 
 const server = createServer((req, res) => {
-  const stream = createReadStream('index.html');
-  return res.writeHead(200, {
-    "content-type": "text/html"
-  }).pipe(stream).end();
+  const stream = createReadStream("index.html");
+  res.writeHead(200, {
+    "content-type": "text/html",
+  });
+  stream.pipe(res);
 });
 const wss = new WebSocketServer({ server });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
   });
 
-  ws.send('server sent' + data);
+  ws.send("server sent" + data);
 });
 
 server.listen(8080);
